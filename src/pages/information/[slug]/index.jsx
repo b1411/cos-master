708d464f38539c61182afaeec9524e2097a24eb7
Rakhmat <kenzhebaev05@gmail.com>
2023-12-25 19:04:26 +0600

    added styles

--- src/pages/information/[slug]/index.jsx
+++ src/pages/information/[slug]/index.jsx
@@ -37,21 +37,39 @@ export default function Information({ informationQuery }) {
                 </Header>
                 <Content className="bg-uibGreen opacity-75">
                     <div className="page-wo-logo">
-                        <div className="relative z-[10] h-full max-w-[900px] px-[20px] mx-auto">
-                            <h2 className="text-center font-bold text-2xl mb-[40px]">{information["полное название"]}</h2>
-                            <div className="text-lg">{information["описание"]}</div>
-                            <div>
-                                {Array.isArray(
-                                    information["контактный номер"],
-                                ) ? (
-                                    information["контактный номер"].map(
-                                        (contact, index) => {
-                                            return <p key={index}>{contact}</p>;
-                                        },
-                                    )
-                                ) : (
-                                    <p>{information["контактный номер"]}</p>
-                                )}
+                        <div className="relative z-[10] h-full max-w-[1000px] px-[20px] mx-auto">
+                            <div className="bg-[#006642] text-white px-5 py-4 rounded-md shadow-md absolute top-1/2 -translate-y-[70%]">
+                                <h2 className="text-center font-bold text-2xl mb-[40px]">
+                                    {information["полное название"]}
+                                </h2>
+                                <div className="text-xl">
+                                    {information["описание"]}
+                                </div>
+                                <div>
+                                    <p className="text-xl mt-4 font-bold">
+                                        Номер для связи:
+                                    </p>
+                                    {Array.isArray(
+                                        information["контактный номер"],
+                                    ) ? (
+                                        information["контактный номер"].map(
+                                            (contact, index) => {
+                                                return (
+                                                    <p
+                                                        className="text-lg"
+                                                        key={index}
+                                                    >
+                                                        {contact}
+                                                    </p>
+                                                );
+                                            },
+                                        )
+                                    ) : (
+                                        <p className="text-lg">
+                                            {information["контактный номер"]}
+                                        </p>
+                                    )}
+                                </div>
                             </div>
                         </div>
                     </div>
